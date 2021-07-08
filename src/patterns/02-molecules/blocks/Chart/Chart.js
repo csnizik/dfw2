353b9e6d9468f79673f6eb53242dcea74076cf68
csnizik <csnizik@tulane.edu>
2021-07-08 14:50:56 -0500

    TopMarkets imports

---
+++ src/patterns/02-molecules/blocks/Chart/Chart.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import GoogleChart from 'react-google-charts';
+
+const Chart = ({
+  chartTitle,
+  width,
+  height,
+  isExpanded,
+  chartType,
+  data,
+  options,
+  rootProps,
+  ...props
+}) => {
+  return (
+    <div>
+      <GoogleChart
+        width={width}
+        height={height}
+        chartType={chartType}
+        loader="Chart is loading..."
+        data={data}
+        options={options}
+        rootProps={rootProps}
+        {...props}
+      />
+    </div>
+  );
+};
+
+export default Chart;
