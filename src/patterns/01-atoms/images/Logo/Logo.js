319aa0bf36b292c5ee0de4d32b2beaa93ea5291e
csnizik <csnizik@tulane.edu>
2021-07-08 14:47:04 -0500

    sidebar imports

---
+++ src/patterns/01-atoms/images/Logo/Logo.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import './Logo.css';
+
+import { ReactComponent as DSFWLogo } from './DestinFWBLogo.svg';
+
+const logoClasses = (size) =>
+  classNames({
+    'w-96 h-96 max-w-full max-h-full': !size || size === 'lg',
+    'w-full h-auto': size === 'full',
+    'w-48 h-48': size === 'md',
+    'w-24 h-24': size === 'sm',
+  });
+
+const logoColor = (color) =>
+  classNames({
+    'text-teal fill-current': !color || color === 'Sea Foam Teal (Primary)',
+    'text-black fill-current': color === 'Black',
+    'text-blue-dark fill-current': color === 'Navy Sky',
+    'text-white-dark fill-current': color === 'Whitewash White',
+  });
+
+
+const Logo = ({ size = 'Large', color }) => {
+  return <DSFWLogo className={`${logoColor(color)} ${logoClasses(size)}`}/>;
+
+};
+
+Logo.propTypes = {
+  size: PropTypes.string,
+};
+export default Logo;
