f94c7ba1e539fc36b57149384d849dc11e502c20
csnizik <csnizik@tulane.edu>
2021-07-12 15:36:09 -0500

    voila

--- src/patterns/04-templates/AtAGlance.js
+++ src/patterns/04-templates/AtAGlance.js
@@ -8,143 +8,145 @@ const AtAGlance = () => {
   const [cardExpanded, setCardExpanded] = useState(false);
 
   return (
-    <Card pathName="/" cardPath={""} cols="3">
-      <h2 className={classNames('hed2', 'col-span-3')}>At a Glance</h2>
-      <div
-        className={classNames(
-          'col-span-3',
-          'grid',
-          'grid-cols-3',
-          'gap-3',
-          'divide-y',
-          'lg:divide-y-0',
-          'lg:divide-x',
-          'divide-gray-300',
-          'pb-4'
-        )}>
+    <div className={classNames('col-span-3')}>
+      <Card pathName="/" cardPath={''}>
+        <h2 className={classNames('hed2', 'col-span-3')}>At a Glance</h2>
         <div
           className={classNames(
             'col-span-3',
-            'lg:col-span-1',
-            'flex',
-            'lg:flex-col',
-            'lg:justify-between',
-            '-space-x-2',
-            'lg:space-0',
-            'items-center'
+            'grid',
+            'grid-cols-3',
+            'gap-3',
+            'divide-y',
+            'lg:divide-y-0',
+            'lg:divide-x',
+            'divide-gray-300',
+            'pb-4'
           )}>
-          <Metric value="12.7%" trend="up" />
-          <h3 className={classNames('hed3')}>Visa Spend Per Visitor</h3>
-        </div>
-
-        <div
-          className={classNames(
-            'col-span-3',
-            'lg:col-span-1',
-            'flex',
-            'lg:flex-col',
-            'lg:justify-between',
-            '-space-x-2',
-            'lg:space-0',
-            'items-center'
-          )}>
-          <Metric value="12.4%" trend="down" />
-          <h3 className={classNames('hed3')}>Visa Consumer Spend</h3>
-        </div>
-
-        <div
-          className={classNames(
-            'col-span-3',
-            'lg:col-span-1',
-            'flex',
-            'lg:flex-col',
-            'lg:justify-between',
-            '-space-x-2',
-            'lg:space-0',
-            'items-center'
-          )}>
-          <Metric value="0.6%" trend="up" />
-          <h3 className={classNames('hed3')}>ROAI</h3>
-        </div>
-      </div>
-
-      <div
-        className={classNames(
-          'col-span-3',
-          'rounded-b-lg',
-          'flex',
-          'flex-col',
-          'flex-grow',
-          'justify-end',
-          'transform',
-          'transition-all',
-          'duration-500',
-          'ease-in-out',
-          '-mx-3',
-          '-mb-3',
-          cardExpanded ? 'h-40' : 'h-12'
-        )}>
-        <div
-          className={classNames(
-            'flex',
-            cardExpanded ? 'h-auto' : 'h-0',
-            'justify-around',
-            'items-center',
-            'overflow-hidden'
-          )}>
-          <div className={classNames('hidden', 'lg:block')}>
-            <div className={classNames('my-6', 'text-center')}>
-              <p className={classNames('font-bold')}>Goal: $240</p>
-              <p>This Season: $240</p>
-              <p>Last Season: $213</p>
-            </div>
-          </div>
-          <div className={classNames('hidden', 'lg:block')}>
-            <div className={classNames('my-6', 'text-center')}>
-              <p className={classNames('font-bold')}>Goal: $552,531,928</p>
-              <p>FY 2020: $502,301,753</p>
-              <p>FY 2019: $573,692,622</p>
-            </div>
-          </div>
-          <div className={classNames('hidden', 'lg:block')}>
-            <div className={classNames('my-6', 'text-center')}>
-              <p>2021: 4.8M</p>
-              <p>2020: 4.7M</p>
-            </div>
-          </div>
-        </div>
-
-        <div
-          className={classNames(
-            'bg-teal',
-            'rounded-b-lg',
-            'h-12',
-            'flex',
-            'justify-center',
-            'py-2'
-          )}>
-          <button
+          <div
             className={classNames(
-              'cursor-pointer',
+              'col-span-3',
+              'lg:col-span-1',
               'flex',
-              'space-between',
+              'lg:flex-col',
+              'lg:justify-between',
+              '-space-x-2',
+              'lg:space-0',
               'items-center'
-            )}
-            onClick={() => setCardExpanded(!cardExpanded)}>
-            Show {cardExpanded ? 'less' : 'more'} information&nbsp;
-            <div
-              className={classNames(
-                'transform',
-                'transition-all',
-                'duration-200',
-                cardExpanded ? 'rotate-0' : 'rotate-180'
-              )}>
-              <Icon type="Triangle" size="s" color="black" />
-            </div>
-          </button>
+            )}>
+            <Metric value="12.7%" trend="up" />
+            <h3 className={classNames('hed3')}>Visa Spend Per Visitor</h3>
+          </div>
+
+          <div
+            className={classNames(
+              'col-span-3',
+              'lg:col-span-1',
+              'flex',
+              'lg:flex-col',
+              'lg:justify-between',
+              '-space-x-2',
+              'lg:space-0',
+              'items-center'
+            )}>
+            <Metric value="12.4%" trend="down" />
+            <h3 className={classNames('hed3')}>Visa Consumer Spend</h3>
+          </div>
+
+          <div
+            className={classNames(
+              'col-span-3',
+              'lg:col-span-1',
+              'flex',
+              'lg:flex-col',
+              'lg:justify-between',
+              '-space-x-2',
+              'lg:space-0',
+              'items-center'
+            )}>
+            <Metric value="0.6%" trend="up" />
+            <h3 className={classNames('hed3')}>ROAI</h3>
+          </div>
         </div>
-      </div>
-    </Card>
+
+        <div
+          className={classNames(
+            'col-span-3',
+            'rounded-b-lg',
+            'flex',
+            'flex-col',
+            'flex-grow',
+            'justify-end',
+            'transform',
+            'transition-all',
+            'duration-500',
+            'ease-in-out',
+            '-mx-3',
+            '-mb-3',
+            cardExpanded ? 'h-40' : 'h-12'
+          )}>
+          <div
+            className={classNames(
+              'flex',
+              cardExpanded ? 'h-auto' : 'h-0',
+              'justify-around',
+              'items-center',
+              'overflow-hidden'
+            )}>
+            <div className={classNames('hidden', 'lg:block')}>
+              <div className={classNames('my-6', 'text-center')}>
+                <p className={classNames('font-bold')}>Goal: $240</p>
+                <p>This Season: $240</p>
+                <p>Last Season: $213</p>
+              </div>
+            </div>
+            <div className={classNames('hidden', 'lg:block')}>
+              <div className={classNames('my-6', 'text-center')}>
+                <p className={classNames('font-bold')}>Goal: $552,531,928</p>
+                <p>FY 2020: $502,301,753</p>
+                <p>FY 2019: $573,692,622</p>
+              </div>
+            </div>
+            <div className={classNames('hidden', 'lg:block')}>
+              <div className={classNames('my-6', 'text-center')}>
+                <p>2021: 4.8M</p>
+                <p>2020: 4.7M</p>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className={classNames(
+              'bg-teal',
+              'rounded-b-lg',
+              'h-12',
+              'flex',
+              'justify-center',
+              'py-2'
+            )}>
+            <button
+              className={classNames(
+                'cursor-pointer',
+                'flex',
+                'space-between',
+                'items-center'
+              )}
+              onClick={() => setCardExpanded(!cardExpanded)}>
+              Show {cardExpanded ? 'less' : 'more'} information&nbsp;
+              <div
+                className={classNames(
+                  'transform',
+                  'transition-all',
+                  'duration-200',
+                  cardExpanded ? 'rotate-0' : 'rotate-180'
+                )}>
+                <Icon type="Triangle" size="s" color="black" />
+              </div>
+            </button>
+          </div>
+        </div>
+      </Card>
+    </div>
   );
 };
 
