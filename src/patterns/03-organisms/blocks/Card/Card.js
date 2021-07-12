5b23332abb33b3a18cf5839854c7c44c1aabfe06
csnizik <csnizik@tulane.edu>
2021-07-12 15:16:42 -0500

    voodoo part deux

--- src/patterns/03-organisms/blocks/Card/Card.js
+++ src/patterns/03-organisms/blocks/Card/Card.js
@@ -2,19 +2,20 @@ import React from 'react';
 import classNames from 'classnames';
 
 const Card = ({ pathName, cardPath, cols, transparent, ...props }) => {
+  const dynamicWidth = `lg:col-span-${cols}`;
+
   return (
     <div
       className={classNames(
         'flex',
         'flex-col',
         'col-span-3',
-        `lg:col-span-${cols}`,
-        // pathName !== cardPath ? `lg:col-span-${cols}` : '',
-        // pathName === cardPath ? 'h-full' : '',
+        pathName !== cardPath ? dynamicWidth : '',
+        pathName === cardPath ? 'h-full' : '',
         transparent ? 'p-0' : 'p-3',
         transparent ? '' : 'rounded-lg',
         transparent ? '' : 'shadow',
-          transparent ? 'bg-transparent' : 'bg-white',
+        transparent ? 'bg-transparent' : 'bg-white'
         // 'hidden'
       )}>
       {props.children}
