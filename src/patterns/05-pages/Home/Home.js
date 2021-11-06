af3f0556791f16f753e0d0a63df84119dbd40a61
csnizik <csnizik@tulane.edu>
2021-11-06 13:35:04 -0500

    max w for main content area

--- src/patterns/05-pages/Home/Home.js
+++ src/patterns/05-pages/Home/Home.js
@@ -10,58 +10,63 @@ import FamilySentiment from '../../04-templates/FamilySentiment';
 import classNames from 'classnames';
 
 const Home = ({ pathName }) => {
-  // const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className={classNames('flex', 'min-h-screen', 'overflow-hidden')}>
-      <Sidebar
-        // sidebarOpen={sidebarOpen}
-        // setSidebarOpen={setSidebarOpen}
-        pathName={pathName}
-      />
+    <div className={classNames('bg-white-dark')}>
       <div
         className={classNames(
-          'relative',
-          'ml-20',
-          'pt-4',
-          'sm:ml-0',
-          'flex-grow',
           'flex',
-          'flex-col',
-          'justify-start',
-          'overflow-y-scroll',
-          'overflow-x-hidden',
-          'bg-white-dark'
+          'min-h-screen',
+          'max-w-screen-2xl',
+          'overflow-hidden'
         )}>
-        <Header pathName={pathName} />
-        <main>
-          <div
-            className={classNames(
-              // 'hidden',
-              'm-6',
-              pathName === '/' ? 'grid' : '',
-              pathName === '/' ? 'grid-cols-3' : '',
-              pathName === '/' ? 'gap-6' : ''
-            )}>
-            {/* Cards */}
-            {pathName === '/' && <AtAGlance pathName={pathName} />}
-            {(pathName === '/' || pathName === '/markets') && (
-              <TopMarkets pathName={pathName} />
-            )}
-            {(pathName === '/' || pathName === '/water') && (
-              <WaterAdventures pathName={pathName} />
-            )}
-            {(pathName === '/' || pathName === '/value') && (
-              <ValueGap pathName={pathName} />
-            )}
-            {(pathName === '/' || pathName === '/confidence') && (
-              <TourismConfidence pathName={pathName} />
-            )}
-            {(pathName === '/' || pathName === '/sentiment') && (
-              <FamilySentiment pathName={pathName} />
-            )}
-          </div>
-        </main>
+        <Sidebar
+          pathName={pathName}
+        />
+        <div
+          className={classNames(
+            'relative',
+            'ml-20',
+            'pt-4',
+            'sm:ml-0',
+            'flex-grow',
+            'flex',
+            'flex-col',
+            'justify-start',
+            // 'overflow-y-scroll', ** re-enable if spacing issues
+            'overflow-x-hidden'
+            // 'bg-white-dark'
+          )}>
+          <Header pathName={pathName} />
+          <main>
+            <div
+              className={classNames(
+                // 'hidden',
+                'm-6',
+                pathName === '/' ? 'grid' : '',
+                pathName === '/' ? 'grid-cols-3' : '',
+                pathName === '/' ? 'gap-6' : ''
+              )}>
+              {/* Cards */}
+              {pathName === '/' && <AtAGlance pathName={pathName} />}
+              {(pathName === '/' || pathName === '/markets') && (
+                <TopMarkets pathName={pathName} />
+              )}
+              {(pathName === '/' || pathName === '/water') && (
+                <WaterAdventures pathName={pathName} />
+              )}
+              {(pathName === '/' || pathName === '/value') && (
+                <ValueGap pathName={pathName} />
+              )}
+              {(pathName === '/' || pathName === '/confidence') && (
+                <TourismConfidence pathName={pathName} />
+              )}
+              {(pathName === '/' || pathName === '/sentiment') && (
+                <FamilySentiment pathName={pathName} />
+              )}
+            </div>
+          </main>
+        </div>
       </div>
     </div>
   );
